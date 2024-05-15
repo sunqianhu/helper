5ad320f157df0793c5c86b5497a4129671640f1e
469661426 <469661426@qq.com>
2024-05-15 18:41:08 +0800

    增加请求类

---
+++ src/Request.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Sunqianhu\Helper;
+
+class Request
+{
+    /**
+     * 得到输入流值
+     * @param $keys
+     * @return array
+     */
+    function input($keys = [])
+    {
+        $input = file_get_contents('php://input');
+        $data = json_decode($input, true);
+        if (empty($data)) {
+            return [];
+        }
+        if (empty($keys)) {
+            return $data;
+        }
+
+        $arrayHandler = new ArrayHandler();
+        return $arrayHandler->getDesignatedKeyList($data, $keys);
+    }
+
+    /**
+     * 得到post值
+     * @param $keys
+     * @return array
+     */
+    function post($keys = [])
+    {
+        $data = $_POST;
+        if (empty($data)) {
+            return [];
+        }
+        if (empty($keys)) {
+            return $data;
+        }
+
+        $arrayHandler = new ArrayHandler();
+        return $arrayHandler->getDesignatedKeyList($data, $keys);
+    }
+
+    /**
+     * 得到get值
+     * @param $keys
+     * @return array
+     */
+    function get($keys = [])
+    {
+        $data = $_GET;
+        if (empty($data)) {
+            return [];
+        }
+        if (empty($keys)) {
+            return $data;
+        }
+
+        $arrayHandler = new ArrayHandler();
+        return $arrayHandler->getDesignatedKeyList($data, $keys);
+    }
+}
