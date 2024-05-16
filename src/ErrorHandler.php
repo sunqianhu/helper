597fa1c2016031d4d507f32d80ed657927bb94f3
469661426 <469661426@qq.com>
2024-05-16 09:18:47 +0800

    优化错误处理器

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -13,13 +13,27 @@ class ErrorHandler
      * @return void
      */
     public function handle($errno, $errstr, $errfile, $errline){
-        $response = new Response();
         $errorTypes = [
-            E_WARNING => '警告',
-            E_NOTICE => '注意'
+            E_ERROR             => '致命错误',
+            E_WARNING           => '警告',
+            E_PARSE             => '解析错误',
+            E_NOTICE            => '注意',
+            E_CORE_ERROR        => '核心错误',
+            E_CORE_WARNING      => '核心警告',
+            E_COMPILE_ERROR     => '编译错误',
+            E_COMPILE_WARNING   => '编译警告',
+            E_USER_ERROR        => '用户错误',
+            E_USER_WARNING      => '用户警告',
+            E_USER_NOTICE       => '用户注意',
+            E_STRICT            => '严格标准',
+            E_RECOVERABLE_ERROR => '可恢复错误',
+            E_DEPRECATED        => '已废弃',
+            E_USER_DEPRECATED   => '用户已废弃',
         ];
         $errorType = isset($errorTypes[$errno]) ? $errorTypes[$errno] : '未知错误';
-        $message = "发生了一个$errorType错误：$errstr，位于文件 $errfile 的第 $errline 行";
+        $errfile = basename($errfile);
+        $message = $errorType.'：'.$errstr.'，在文件'.$errfile.'的第'.$errline.'行';
+        $response = new Response();
         echo $response->error($message);
         exit;
     }
