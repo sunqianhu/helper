8c5d739fca22c8f9a2435548190d0f2a269d5d7e
469661426 <469661426@qq.com>
2024-07-13 09:56:54 +0800

    优化创建压缩文件

--- src/ZipArchive.php
+++ src/ZipArchive.php
@@ -11,21 +11,22 @@ class ZipArchive
      * 创建压缩文件
      * @param $fullPath
      * @param $files
-     * @return void
+     * @return BaseZipArchive
      * @throws Exception
      */
     public function createZip($fullPath, $files){
         if(empty($files)){
             throw new Exception('压缩文件不能为空');
         }
-        $baseZipArchive = new BaseZipArchive();
-        $createResult = $baseZipArchive->open($fullPath, BaseZipArchive::CREATE | BaseZipArchive::OVERWRITE);
+        $zipArchive = new BaseZipArchive();
+        $createResult = $zipArchive->open($fullPath, BaseZipArchive::CREATE | BaseZipArchive::OVERWRITE);
         if ($createResult !== true) {
             throw new Exception($createResult);
         }
         foreach ($files as $file) {
-            $baseZipArchive->addFile($file['full_path'], $file['entry_name']);
+            $zipArchive->addFile($file['full_path'], $file['entry_name']);
         }
-        $baseZipArchive->close();
+        $zipArchive->close();
+        return $zipArchive;
     }
 }
