1938b33c6914ad1f3232329ee5011699dd82e0c0
sunqianhu <469661426@qq.com>
2025-07-14 17:38:14 +0800

    增加富文本处理类

---
+++ src/RichTextHandler.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sunqianhu\Helper;
+
+class RichTextHandler
+{
+    /**
+     * 转保持格式的纯文本
+     * @param $richText
+     * @return string
+     */
+    public function toMaintainFormatPlainText($richText)
+    {
+        $richText = preg_replace('#<(script|style)\b[^>]*\s*>.*?</\s*\1>#si', '', $richText);
+        $richText = preg_replace('/<!--.*?-->/s', '', $richText);
+
+        $blockTags = [
+            'div', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'li', 'tr', 'th', 'td', 'blockquote', 'pre', 'section'
+        ];
+        foreach ($blockTags as $tag) {
+            $richText = preg_replace('#<' . $tag . '\b[^>]*?>#i', "", $richText);
+            $richText = preg_replace('#</' . $tag . '\b[^>]*?>#i', "\n", $richText);
+        }
+        $richText = preg_replace('#<br\s*/?>#i', "\n", $richText);
+
+        $richText = strip_tags($richText);
+        $richText = preg_replace("/[ \t]+/", ' ', $richText);
+        $richText = html_entity_decode($richText, ENT_QUOTES, 'UTF-8');
+
+        return trim($richText);
+    }
+
+    /**
+     * 纯文本转富文本（HTML）
+     * @param string $plainText
+     * @return string
+     */
+    public function toRichText($plainText)
+    {
+        $plainText = trim($plainText);
+        $plainText = htmlspecialchars($plainText, ENT_QUOTES, 'UTF-8');
+
+        $paragraphs = preg_split("/\n\s*\n/", $plainText);
+
+        $html = '';
+        foreach ($paragraphs as $para) {
+            $para = nl2br($para, true);
+            $html .= "<p>{$para}</p>\n";
+        }
+
+        return $html;
+    }
+}
